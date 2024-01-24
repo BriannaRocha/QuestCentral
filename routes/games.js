ebdf345f777f0caf29596495513b595b9d9e9c2b
BriannaRocha <rochabrianna17@gmail.com>
2024-01-24 02:33:18 -0500

    add routes to games.js

--- routes/games.js
+++ routes/games.js
@@ -1,15 +1,29 @@
 import { Router } from 'express'
 import * as gamesCtrl from '../controllers/games.js'
-import { isLoggedIn } from "../middleware/middleware.js";
+import { isLoggedIn } from "../middleware/middleware.js"
 
 const router = Router()
 
 // GET localhost:3000/games
 router.get('/', gamesCtrl.index)
-// GET localhost:3000/games
-router.get('/:gameId', gamesCtrl.show)
 // POST localhost:3000/games
 router.post('/', isLoggedIn, gamesCtrl.create)
+// GET localhost:3000/games
+router.get('/:gameId', gamesCtrl.show)
+// GET localhost:3000/games/:gameId/edit
+router.get('/:gameId/edit', isLoggedIn, gamesCtrl.edit)
+// PUT localhost:3000/games/:gameId
+router.put('/:gameId', isLoggedIn, gamesCtrl.update)
+// DELETE localhost:3000/games/:gameId
+router.delete('/:gameId', isLoggedIn, gamesCtrl.delete)
+// POST localhost:3000/games/:gameId/notes
+router.post('/:gameId/notes', isLoggedIn, gamesCtrl.addNote)
+// POST localhost:3000/games/:gameId/notes
+router.get('/:gameId/notes/:noteId/edit', isLoggedIn, gamesCtrl.editNote)
+// PUT localhost:3000/games/:gameId/notes/:noteId
+router.put('/:gameId/ntoes/:noteId', isLoggedIn, gamesCtrl.updateNote)
+// DELETE localhost:3000/games/:gameId/notes/:noteId
+router.delete('/:gameId/notes/:noteId', isLoggedIn, gamesCtrl.deleteNote)
 
 export {
   router
