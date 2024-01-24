672c16e4f43a2fa8981d72f4075fe4b91e5b7d40
BriannaRocha <rochabrianna17@gmail.com>
2024-01-23 23:39:54 -0500

    add profiles controller and add to server.js

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ import'./config/passport.js'
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
 import { router as gamesRouter } from './routes/games.js'
+import { router as profilesRouter } from './routes/profiles.js'
 import { router as reviewsRouter } from './routes/reviews.js'
 
 // create the express app
@@ -64,6 +65,7 @@ app.use(passDataToView)
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
 app.use('/games', gamesRouter)
+app.use('/profiles', profilesRouter)
 app.use('/reviews', reviewsRouter)
 
 // catch 404 and forward to error handler
