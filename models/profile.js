22e25103d9db03fe71292b558b5d88dd56f258f9
BriannaRocha <rochabrianna17@gmail.com>
2024-01-25 18:44:11 -0500

    add profile model

--- models/profile.js
+++ models/profile.js
@@ -2,9 +2,22 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
+const gamertagSchema = new Schema({
+  name: String,
+  platform: {
+    type: String,
+    enum: ['Apple', 'Epic Games', 'Google', 'Nintendo', 'Playstation', 'Steam', 'Ubisoft', 'Xbox'],
+    default: 'Steam'
+  },
+}, {
+  timestamps: true
+})
+
 const profileSchema = new Schema({
   name: String,
+  age: Number,
   avatar: String,
+  gamertags: [gamertagSchema]
 }, {
   timestamps: true
 })
